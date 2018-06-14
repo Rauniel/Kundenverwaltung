c7363c22aecd2f9931afecaff8ee61412adb8e17
Aragorn Kopp <aragorn.kopp@gibmit.ch>
2018-06-14 11:38:53 +0200

    Added crm_buissnes Added crm_persistence Added crm_web-client

---
+++ crm_web-client/src/ch/ilv/crm/webclient/views/CustomerReadView.java
@@ -0,0 +1,53 @@
+/**
+ * This CustomerReadView implements a view by using Giniality´s WebApp-Framework.
+ */
+
+package ch.ilv.crm.webclient.views;
+
+import com.giniality.education.webappframework.view.AbstractHtmlView;
+
+import ch.ilv.crm.business.ICustomer;
+
+public class CustomerReadView extends AbstractHtmlView {
+		
+	/**
+	 * For informations look at this framework in:
+	 * @see com.giniality.education.webappframework.view.AbstractHtmlView#createPageContents()
+	 * 
+	 * @return text The text which is shown on the website
+	 */
+	public String createPageContents() {
+		
+		String text = "";
+		
+		Object displayObject = getDisplayObject();
+		
+		if (displayObject != null) {
+			ICustomer selectedCustomer = (ICustomer)((Object[])displayObject)[0];
+			int customerId = (int)((Object[])displayObject)[1];
+			text = "<h1>Der gesuchte Kunde wurde erfolgreich gefunden</h1>";
+			text += "<h3>Id: " + customerId +  "</h3>";
+			text += "<h3>Name: " + selectedCustomer.getName() + "<br />\n Vorname: " + selectedCustomer.getForename() + "</h3>";
+		} else {
+			text = "<h1>Der Kunde wurde nicht gefunden!</h1>";
+		}
+		
+		return text;
+	}
+
+	/**
+	 * For informations look at this framework in:
+	 * @see com.giniality.education.webappframework.view.AbstractHtmlView#getTitle()
+	 */
+	public String getTitle() {
+		return "crm-app";
+	}
+
+	/**
+	 * For informations look at this framework in:
+	 * @see com.giniality.education.webappframework.view.AbstractHtmlView#getAliasName()
+	 */
+	public String getAliasName() {
+		return "ReadCustomerView";
+	}
+}
